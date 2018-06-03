9bbc6b9cdbcc1ce01dab6a57278a29eea222a11b
Ben Teppers <ben.teppers@icloud.com>
2018-06-03 17:13:01 +0200

    added implementation of protocolservice.

--- src/main/java/com/database/PizzaOrderService.java
+++ src/main/java/com/database/PizzaOrderService.java
@@ -1,18 +1,9 @@
 package com.database;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
 import com.entities.PizzaOrder;
 
-public class PizzaOrderService {
+public class PizzaOrderService implements ProtocolService  {
 
-	private final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("pizzahouse");
-	private final EntityManager EM = EMF.createEntityManager();
-	private final EntityTransaction TX = EM.getTransaction();
-	
 	public void createOrder(PizzaOrder order) {
 		TX.begin();
 		EM.persist(order);
